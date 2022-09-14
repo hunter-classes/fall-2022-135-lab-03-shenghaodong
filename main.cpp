2b953451b30e8880ee78aee65102280ed64ac9d3
Shenghao Dong <shenghaodong18@gmail.com>
2022-09-14 17:50:44 -0400

    worked on function in main. Move to reservoir.cpp later

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 // add your code as appropriate 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
 
 int main()
 {
-  
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  if (fin.fail()) {
+    std::cerr << "File cannot be opened for reading." << std::endl;
+    exit(1); // exit if failed to open the file
+  }
+  std::string junk;
+  getline(fin, junk);
 
+  std::string userInput = "12/29/2018";
+  std::string date;
+  double eastSt;
+  double eastEl;
+  double westSt;
+  double westEl;
+  while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+    // this loop reads the file line-by-line
+    // extracting 5 values on each iteration 
+    
+    fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+    //ignoring the remaining columns 
+
+    // for example, to print the date and East basin storage:
+    if(date == userInput){
+      std::cout << date << " " << eastSt << std::endl;
+    }
+    //std::cout << date << " " << eastSt << std::endl;
+  } 
   
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,4 @@
 // add your code
+std::string compare_basins(std::string date){
+    
+}
